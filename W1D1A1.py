3bbfaa6537b1f4eba0397401417368058724677b
KNAlak <99671857+KNAlak@users.noreply.github.com>
2022-02-16 20:41:10 +0300

    W1D1

--- W1D1A1.py
+++ W1D1A1.py
@@ -1,13 +1,6 @@
-
-
-
-
-
-
-
 x = float(input('Please input the length of the room: '))
 y = float(input('Please input the width of the room: '))
 z =  float(input('Please input the length of the door: '))
 print(f'room area that need to be covered: ', x*y)
 print(f'room perimeter that need to be installed baseboard :', 2*(x+y)-z)
-print('its done')
+
